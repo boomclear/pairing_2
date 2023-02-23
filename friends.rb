42e96a6a925e5f2b53523dbbcbd0aeb73a38649f
Austin Woods <austinwoods99@gmail.com>
2023-02-23 14:53:20 -0500

    Add another array with method called on it

--- friends.rb
+++ friends.rb
@@ -3,3 +3,7 @@ arrays = ["Will", "Andy", "Jimmy", "Austin", "Kaitlyn"]
 p arrays[2]
 p arrays[4]
 p arrays[0]
+
+array_places = ["New York", "Chicago", "Orlando", "San Francisco", "Santa Fe"]
+
+p array_places.pop
